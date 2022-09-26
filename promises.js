c39c0bbfa087fdddddd54106011586adc12b23bc
Robinson <robinadoro@gmail.com>
2022-09-26 11:51:22 +0300

    add async-await

--- promises.js
+++ promises.js
@@ -27,9 +27,7 @@ function createPost (post){
                 reject('Error: Something went wrong');
             }
         }, 2000);
-    })
-
-    
+    })    
 }
 
 // createPost({title:'post three',body:'this is post three'})
@@ -38,14 +36,14 @@ function createPost (post){
 
 // promise.all
 
-const promise1 = Promise.resolve('Hello world');
-const promise2 = 10;
-const promise3 = new Promise((resolve, reject) => 
-setTimeout(resolve, 2000, 'Goodbye'));
+// const promise1 = Promise.resolve('Hello world');
+// const promise2 = 10;
+// const promise3 = new Promise((resolve, reject) => 
+// setTimeout(resolve, 2000, 'Goodbye'));
 
-const promise4 = fetch
-('https://jsonplaceholder.typicode.com/users')
-.then(res => res.json())
+// const promise4 = fetch
+// ('https://jsonplaceholder.typicode.com/users')
+// .then(res => res.json())
 
-Promise.all([promise1, promise2, promise3, promise4])
-.then(values => console.log(values))
+// Promise.all([promise1, promise2, promise3, promise4])
+// .then(values => console.log(values))
